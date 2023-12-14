f8c30dc2dbc0237277f2be301f35acc634002308
Alexis Lahouze <alexis@sysnove.fr>
2023-12-14 10:00:39 +0100

    Update dependency.

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ setup(
     author_email="maethor+pip@subiron.org",
     description="Base class to manage models with ldapom.",
     long_description=read('README.md'),
-    install_requires=['ldapom==0.12.0'],
     long_description_content_type="text/markdown",
+    install_requires=['ldapom>=0.12.1'],
     include_package_data=True,
     url='http://github.com/sysnove/ldapom-model',
     classifiers=[
